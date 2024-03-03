cbff8650c31116df86bed0da54e406c1e75f1434
Teodoro Pedro <teosoares10@gmail.com>
2024-03-03 15:46:18 +0100

    feat: add routes to browser router
    create a browser router and define path, element, error element and children for the routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "./root";
+import ErrorPage from "../error-page";
+import HomePage from "./home-page";
+import AboutPage from "./about-page";
+import ShopPage from "./shop-page";
+import BlogPage from "./blog";
+import ContactPage from "./contact";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/",
+                element: <HomePage />
+            },
+            {
+                path: "/shop",
+                element: <ShopPage />
+            },
+            {
+                path: "/about",
+                element: <AboutPage />
+            },
+            {
+                path: "/blog",
+                element: <BlogPage />
+            },
+            {
+                path: "/contact",
+                element: <ContactPage />
+            }
+        ]
+    }
+])
