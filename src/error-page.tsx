69532dc2992c985b02497e53332c0137d9709c97
Teodoro Pedro <teosoares10@gmail.com>
2024-03-03 13:54:30 +0100

    feat: create a error page
    show page error when something went wrong with the page

---
+++ src/error-page.tsx
@@ -0,0 +1,18 @@
+import { useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+        {/* @ts-expect-error */}
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+}
